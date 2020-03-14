44815af6f039b66ea347c3478b8e2a4538eb61f6
Moti Bartov <motib@tikalk.com>
2020-03-14 07:48:21 +0200

    eslint

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.checkId = (req, res, next, val) => {
-  console.log('Tour id: ' + val);
+  console.log(`Tour id: ${val}`);
   const id = val * 1;
-  const tour = tours.find(tour => tour.id === id);
+  const tour = tours.find(t => t.id === id);
   //   console.log(tour);
 
   if (tour === undefined) {
@@ -51,7 +51,7 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find(tour => tour.id === id);
+  const tour = tours.find(t => t.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -67,7 +67,7 @@ exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), () => {
     res.status(201).json({
       status: 'success',
       data: {
--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -31,7 +31,7 @@ exports.checkId = (req, res, next, val) => {
 };
 
 exports.getUser = (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
   console.log(`${id}`);
 
   const user = users.find(el => el._id === id);
@@ -49,9 +49,10 @@ exports.getUser = (req, res) => {
 
 exports.createUser = (req, res) => {
   const newId = users[users.length - 1].id + 1;
-  const newUser = Object.assign({ id: newId }, req.body);
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const newUser = { id: newId, ...req.body };
   users.push(newUser);
-  fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(users), err => {
+  fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(users), () => {
     res.status(201).json({
       status: 'success',
       data: {
@@ -64,8 +65,7 @@ exports.createUser = (req, res) => {
 exports.updateUser = (req, res) => {
   //TODO: handle update a tour ...
 
-  console.log('Update user: ' + req.params.id);
-  const user = users.find(el => el.id === req.params.id);
+  console.log(`Update user: ${req.params.id}`);
 
   res.status(200).json({
     status: 'success',
@@ -78,8 +78,7 @@ exports.updateUser = (req, res) => {
 exports.deleteUser = (req, res) => {
   //TODO: handle update a tour ...
 
-  console.log('Delete user: ' + req.params.id);
-  const user = users.find(el => el.id === req.params.id);
+  console.log(`Delete user: ${req.params.id}`);
 
   res.status(204).json({
     status: 'success',
