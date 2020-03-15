a94b757f2d34345f1f0929a6c4335b589273a75a
Moti Bartov <motib@tikalk.com>
2020-03-15 11:59:44 +0200

    Use mongoose to connect remote mongodb server

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,20 @@
 //Server
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// const LOCAL_DATA = process.env.DATABASE_LOCAL;
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection established'));
+
 const app = require('./app');
 
 console.log(process.env.NODE_ENV);
