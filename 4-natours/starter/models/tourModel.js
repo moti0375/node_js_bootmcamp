ad9a3f4fe7ca4cd02c6f11ac572f0deec8845670
Moti Bartov <motib@tikalk.com>
2020-03-20 16:15:07 +0200

    104 - Document middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A Tour must have a duration']
@@ -60,6 +62,24 @@ const tourSchema = new mongoose.Schema(
 );
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+//Document middleware, runs before save and create
+tourSchema.pre('save', function(next) {
+  // console.log(`Save middleware ${JSON.stringify(this)}`);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function(next) {
+  console.log(`Another pre.save middleware`);
+  next();
+});
+
+//Document middleware, runs after save and create actions
+tourSchema.post('save', function(doc, next) {
+  console.log(`Post middleware: ${JSON.stringify(doc)}`);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
