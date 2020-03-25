2f41ed4a0d672fbeb86f0ceaff1629aded4b3d8f
Moti Bartov <motib@tikalk.com>
2020-03-25 14:59:43 +0200

    118 - Handling astError and ValidationError

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,3 +1,16 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = error => {
+  console.log('handleCastErrorDB: was called');
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = error => {
+  console.log('handleValidationErrorDB was called');
+  return new AppError(error.message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -15,21 +28,28 @@ const sendErrorProd = (err, res) => {
     });
   } else {
     console.error('💥 ERROR:', err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong'
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+  console.error(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    } else if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
