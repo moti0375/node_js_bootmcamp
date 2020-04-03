da024386d04906207f0ca301255707a6c6fa1d46
Moti Bartov <motib@tikalk.com>
2020-04-03 23:09:24 +0300

    150 - Embedding Tour guide model

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -103,7 +104,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
@@ -115,6 +117,16 @@ tourSchema.virtual('durationWeeks').get(function() {
 tourSchema.pre('save', function(next) {
   // console.log(`Save middleware ${JSON.stringify(this)}`);
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => {
+    console.log(`Mapping ${id}`);
+    return await User.findById(id);
+  });
+
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
