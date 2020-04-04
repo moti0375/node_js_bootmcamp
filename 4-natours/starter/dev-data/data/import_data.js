79056a9e03d5c47cd50c061ff4a879d595939dd0
Moti Bartov <motib@tikalk.com>
2020-04-04 22:23:40 +0300

    166 - Import data js

--- 4-natours/starter/dev-data/data/import_data.js
+++ 4-natours/starter/dev-data/data/import_data.js
@@ -4,6 +4,8 @@ dotenv.config('../../.env');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 // const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 // console.log(DB);
@@ -12,21 +14,23 @@ const DB =
   'mongodb+srv://natours-remote-db:uORaJ5YvcPFsComh@natours-app-course-rpoqx.mongodb.net/natours?retryWrites=true&w=majority';
 
 const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync('./reviews.json', 'utf-8'));
 
 // console.log(tours);
 
-const saveTours = async t =>
-  await Tour.create(t).then(result => {
-    console.log('Tours saved to db');
-    console.log(result);
-    process.exit();
-  });
+const saveData = async () => {
+  // await Tour.create(tours);
+  // await User.create(users, { validateBeforeSave: false });
+  await Review.create(reviews, { validateBeforeSave: false });
+  process.exit();
+};
 
-const deleteTours = async () => {
-  await Tour.deleteMany().then(result => {
-    console.log('Tours deleted');
-    process.exit();
-  });
+const deleteMany = async () => {
+  await Tour.deleteMany();
+  await User.deleteMany();
+  await Review.deleteMany();
+  process.exit();
 };
 
 mongoose
@@ -38,9 +42,9 @@ mongoose
   .then(() => {
     try {
       if (process.argv[2] === '--import') {
-        saveTours(tours);
+        saveData();
       } else if (process.argv[2] === '--delete') {
-        deleteTours();
+        deleteMany();
       } else {
         console.log('Nothing done..');
       }
