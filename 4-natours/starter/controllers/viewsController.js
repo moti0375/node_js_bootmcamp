64459408d3d70b922621e2df8036accd73e57c2e
Moti Bartov <motib@tikalk.com>
2020-04-09 16:21:31 +0300

    181 - Card template

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,16 +1,25 @@
 const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   console.log('getOverView');
-  res.status(200).render('base', {
-    tour: 'Eilat Israel',
-    user: 'Moti Bartov'
+  //1. Get all tours from our collection
+  const tours = await Tour.find();
+  //2. Build template
+
+  //3. Render the template using data from step 1
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours: tours
   });
+  next();
 });
 
-exports.getTourDetails = catchAsync(async (req, res) => {
+exports.getTourDetails = catchAsync(async (req, res, next) => {
   console.log('getTourDetails');
   res.status(200).render('tour', {
     title: 'Forest Hiker'
   });
+
+  next();
 });
