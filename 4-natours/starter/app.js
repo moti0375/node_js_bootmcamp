5a5fcaa98aee8cac592ef6bd13355d0648c8bde3
Moti Bartov <motib@tikalk.com>
2020-04-09 15:18:56 +0300

    179 - Extending pug files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -72,6 +72,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'Forest Hiker'
+  });
+});
+
 app.use('/api/v1/users', usersRouter); //UsersRouter middleware
 app.use('/api/v1/tours', toursRouter); //ToursRouter middleware
 app.use('/api/v1/reviews', reviewRouter); //Review middleware
